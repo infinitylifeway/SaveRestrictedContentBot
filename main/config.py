79d1774e9438b817bee4ba47d29255e74c6b4af3
Suraj kr Chauhan <54215056+infinitylifeway@users.noreply.github.com>
2025-05-16 01:14:46 +0530

    Delete main/config.py

--- main/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    BOT_TOKEN = "7481757314:AAEotMzs7stso6DCJRLWWTrmebjk66OoYeU"
-    API_ID = 7894393
-    API_HASH = "a4553acb1b780df632a78fd13caae7ac"
-    BOT_USERNAME = "copyrightteamX_bot"
-    APP_SHORT_NAME = "copyright"
-    MONGODB_URI = "mongodb+srv://surajkumarchauhan003:surajkumarchauhan003@cluster0.nskvlmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    OWNER_ID = 1334519670
